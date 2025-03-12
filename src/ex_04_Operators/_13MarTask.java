6a13d471bdb1f0eb3c53750ffa2da9edad4e3125
Shivam Volvoicar <110778499+Shivameme17@users.noreply.github.com>
2025-03-13 01:55:42 +0530

    Task 13 march

--- src/ex_04_Operators/_13MarTask.java
+++ src/ex_04_Operators/_13MarTask.java
@@ -3,7 +3,7 @@ package ex_04_Operators;
 public class _13MarTask {
     public static void main(String[] args) {
         double x = 10, y = 10, z = 10;
-        double result = Math.cbrt(x * x + y * y - Math.abs(z));
+        double result = Math.cbrt(Math.pow(x, 2) + Math.pow(y, 2) - Math.abs(z));
         System.out.println("Result: " + result);
     }
 }
