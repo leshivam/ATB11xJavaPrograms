bbb685460093e91099e635bfbcdd80e1dffcaecd
Shivam Volvoicar <110778499+Shivameme17@users.noreply.github.com>
2025-03-13 01:54:13 +0530

    Task 13 march

--- src/ex_04_Operators/_13MarTask.java
+++ src/ex_04_Operators/_13MarTask.java
@@ -1,6 +1,6 @@
-package ex_03_Literals;
+package ex_04_Operators;
 
-public class Lab035_PremitiveDataType {
+public class _13MarTask {
     public static void main(String[] args) {
         double x = 10, y = 10, z = 10;
         double result = Math.cbrt(x * x + y * y - Math.abs(z));
