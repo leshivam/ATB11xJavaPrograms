70c8c17433a02cc1de7a8236c647d7cc1044e54e
Shivam Volvoicar <110778499+Shivameme17@users.noreply.github.com>
2025-03-18 21:31:16 +0530

    Task 12 march

---
+++ src/ex_04_Operators/Lab_053_Interview.java
@@ -0,0 +1,9 @@
+package ex_04_Operators;
+
+public class Lab_053_Interview {
+    public static void main(String[] args) {
+        long l =10l;
+        String s = "name";
+
+    }
+}
---
+++ src/ex_04_Operators/Lab_054_Interview.java
@@ -0,0 +1,15 @@
+package ex_04_Operators;
+
+
+
+public class Lab_054_Interview {
+    public static void main(String[] args) {
+
+
+    char a1 = 'A';
+    char b1 = 'B';
+        System.out.println(a1 + b1);
+    }
+
+
+ }
